9be89be3aba1eb0f99bd73707302dfc2dca54ce1
GK <george@barcelonacodeschool.com>
2022-02-10 13:22:02 +0100

    heroku deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3040;
+const port = process.env.PORT || 3040;
 require('dotenv').config();
 // =============== BODY PARSER SETTINGS =====================
 const bodyParser = require('body-parser');
